cb4ce3fcb92b934c1f7795855340e16f02a1ba6e
Guilherme Machado <guilherme.machado@diwe.com.br>
2022-10-19 22:32:00 -0300

    chore: setup eslint with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,28 @@
 module.exports = {
   env: {
-    es2021: true,
-    node: true
+    browser: true,
+    es6: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly',
+  },
+  parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ['react', 'prettier'],
   rules: {
-  }
-}
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', {extensions: ['.jsx', '.js']}],
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'no-console': ['error', {allow: ['tron']}],
+  },
+};
